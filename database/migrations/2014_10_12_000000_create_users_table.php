aad46dc6b3dbd674536d9894e57e1ce8deede0f9
Janrey Maligro <malricjan25@mail.com>
2019-11-14 13:23:56 +0800

    db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,11 +17,18 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('is_admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+        DB::table('users')->insert([
+            'name'      =>  'Janrey Maligro',
+            'email'     =>  'admin@admin.com',
+            'is_admin'  =>  true,
+            'password'  =>  bcrypt('12345678'),
+        ]);
     }
 
     /**
